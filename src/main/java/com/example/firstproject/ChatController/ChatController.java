6e05b7359791de1cb9c9e3803a056a65c43b6e12
codeBox <148044218+hongjunpyo123@users.noreply.github.com>
2024-11-09 17:46:09 +0900

    feat: 랩실 ai완선

--- src/main/java/com/example/firstproject/ChatController/ChatController.java
+++ src/main/java/com/example/firstproject/ChatController/ChatController.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.ChatController;
 
 import com.example.firstproject.Dto;
+import com.example.firstproject.chatMessage.ChatMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ai.anthropic.AnthropicChatModel;
@@ -10,29 +11,38 @@ import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 import java.util.Map;
 
-@RestController
+@Controller
 public class ChatController {
 
     private final AnthropicChatModel chatModel;
+    private ChatMessage lapdata = new ChatMessage(
+            "연구실_이름 : pl 랩실, " +
+            "교수:홍길동 교수님, " +
+            "연구_분야:[머신러닝, 프로그래밍]" +
+            "구성원:[홍길동, 오하준, 김수빈, 정민수, 이지현 등등]");
 
     @Autowired
     public ChatController(AnthropicChatModel chatModel) {
         this.chatModel = chatModel;
     }
 
+    @GetMapping("/")
+    public String enter(){
+        return "chat";
+    }
     @GetMapping("/ai")
-    public Dto generate(@RequestParam String message, Model model) {
-        Dto dto = new Dto();
-        dto.setReponse_message(chatModel.call(message));
-
-        return dto;
+    public String generate(@RequestParam String message, Model model) {
+        if(message.isEmpty()){
+            model.addAttribute("response", "질문 주시면 성실히 답변드릴께요!");
+            return "chat";
+        }
+        message = lapdata.getMessage() + "모든 답변은 방금 올린 데이터 안에서만 가능해, 이외의 질문에는 답변 할 수 없어요! 출력, 답변은 최대한 간결하게하고 불필요한 말은 하지마, 사용자 질문 : " + message;
+        model.addAttribute("response", chatModel.call(message));
+        return "chat";
     }
 }
---
+++ src/main/java/com/example/firstproject/chatMessage/ChatMessage.java
@@ -0,0 +1,20 @@
+package com.example.firstproject.chatMessage;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class ChatMessage {
+    private String chatMessage;
+
+    public ChatMessage(String message){
+        this.chatMessage = message;
+    }
+
+    public void setMessage(String message){
+        this.chatMessage = message;
+    }
+
+    public String getMessage(){
+        return this.chatMessage;
+    }
+}
